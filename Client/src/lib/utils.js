8f0518654bda843b98325013ba4fefbfbb3c976a
Khouchane Mahmoud <97807779+kh-mahmoud@users.noreply.github.com>
2025-07-19 02:07:54 +0200

    Update utils.js

--- Client/src/lib/utils.js
+++ Client/src/lib/utils.js
@@ -5,7 +5,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
 
-export const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-preview-04-17" });
+export const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 
 // Helper function to convert Blob to base64 string
